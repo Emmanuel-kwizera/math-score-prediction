5aab42fe63d789eb04158d49b22631f1f7997e78
Emmanuel-kwizera <kwizemmy2004@gmail.com>
2025-07-21 18:11:49 +0200

    feat: completed fast api implementation

---
+++ api/main.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+import joblib
+import numpy as np
+import pandas as pd
+import os
+
+app = FastAPI()
+modelPath = os.path.join(os.path.dirname(__file__), 'math_score_model.pkl')
+
+class StudentData(BaseModel):
+    gender: str
+    race_ethnicity: str
+    parental_level_of_education: str
+    lunch: str
+    test_preparation_course: str
+    reading_score: float
+    writing_score: float
+
+def predict_math_score(data: StudentData):
+    # Load the model
+    model = joblib.load(modelPath)
+    
+    # Prepare the input data
+    input_data = pd.DataFrame([{
+        'gender': data.gender,
+        'race_ethnicity': data.race_ethnicity,
+        'parental_level_of_education': data.parental_level_of_education,
+        'lunch': data.lunch,
+        'test_preparation_course': data.test_preparation_course,
+        'reading_score': data.reading_score,
+        'writing_score': data.writing_score
+    }])
+
+    # Make predictions
+    predictions = model.predict(input_data)
+    return predictions[0]
+
+@app.post("/predict")
+def predict(data: StudentData):
+    return {"predicted_math_score": predict_math_score(data)}
